0c3c323554b0dcc23cbd0029d3964c96dd462d9e
Yan Musikhin <yanmusikhin@gmail.com>
2023-03-22 14:04:06 +0400

    feat: add global styles

---
+++ src/theme/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  white: '#F3F4F6',
+  white100: '#E2E3E5',
+  white500: '#c4c4c4',
+  baseBlack: '#000000',
+  baseWhite: '#ffffff',
+  iris100: '#5D5FEF',
+  grey: '#4D5562',
+  grey900: '#111827',
+}
+export const boxShadow = {
+  baseShadow: `0 2px 4px rgba(0, 0, 0, 0.06), 0 4px 6px rgba(0, 0, 0, 0.1)`,
+}
